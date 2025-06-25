f10d1172703522ad6191afbc88652e3358bc2116
Yusuf Bolden <ajb2316@columbia.edu>
2025-06-25 10:41:46 -0400

    Commits initial file structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold mt-4 text-center'>Recipe Discovery App</h1>
+    </>
+  )
+}
+
+export default App
