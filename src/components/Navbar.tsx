ad0e014c195e537366559797da8961785bc452dd
Yusuf Bolden <ajb2316@columbia.edu>
2025-06-26 15:31:03 -0400

    Modifies styles to Navbar elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
 
   return (
     <nav className="bg-blue-300 text-white px-4 py-3 flex flex-col sm:flex-row sm:justify-between items-center gap-4">
-      <Link to="/" className="text-xl font-bold">
+      <Link to="/" className="text-xl font-bold hover:underline">
         Recipe Finder
       </Link>
       <form onSubmit={handleSubmit} className="flex items-center gap-2">
@@ -24,7 +24,7 @@ export const Navbar = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search recipes..."
-          className="px-3 py-1 rounded text-black font-bold border-2 border-black-500"
+          className="px-3 py-1 rounded text-black font-bold border-2 border-gray-300"
         />
         <button
           type="submit"
@@ -33,7 +33,7 @@ export const Navbar = () => {
           Search
         </button>
       </form>
-      <Link to="/favorites" className="hover:underline">
+      <Link to="/favorites" className="font-bold text-xl hover:underline">
         Favorites
       </Link>
     </nav>
